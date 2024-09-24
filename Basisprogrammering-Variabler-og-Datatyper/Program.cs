c416cc3d4d040dbf3449cceb691f3074a5cc6752
Lucas-eadania <lupa6@student.eadania.dk>
2024-09-24 12:03:04 +0200

    Projekt startup

---
+++ Basisprogrammering-Variabler-og-Datatyper/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basisprogrammering_Variabler_og_Datatyper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
